86eb97bfd2c21e0c369e873b986f2b9d78c56338
apykwan <apykwan@hotmail.com>
2022-06-12 16:14:30 -0700

    create project list rendering instead of hardcoded

---
+++ javascript/projectList.js
@@ -0,0 +1,86 @@
+const projectList = [
+    {
+        name: 'Shop it',
+        icon: 'fa-solid fa-cart-shopping',
+        link: 'https://apkwan-shopit.herokuapp.com',
+        highlights: ['React.js', 'Bootstrap', 'Redux', 'Node.js', 'MongoDB', 'Search Bar', 'Forget / Reset password', 'Restful APIS', 'Authentication'],
+        description: 'An Imitation of Amazon, users may search the products, add them to the shopping cart and finally make the payment.'
+    }, 
+    {
+        name: 'The Clothing',
+        icon: 'fa-solid fa-shirt',
+        link: 'https://apkwan-clothing.herokuapp.com',
+        highlights: ['React.js', 'Redux Saga', 'Node.js', 'MongoDB', 'SASS', 'Gmail login', 'Firebase database', 'Stripe credit card payment'],
+        description: 'Simulating online clothing store. Patrons may sign in with Google, add items to cart, and make payment.'
+    },
+    {
+        name: 'Subscription',
+        icon: 'fa-solid fa-dollar-sign',
+        link: 'https://apkwan-subscription.firebaseapp.com',
+        highlights: ['React.js', 'Bootstrap', 'Node.js', 'MongoDB', 'Facebook / Gmail Login', 'Forget / Reset password', 'Restful APIS', 'Authentication',, 'Stripe credit card payment'],
+        description: 'Need monthly advice on stocks? Login with your facebook ID or simply create an account. You may subscribe our services through Stripe.'
+    }, 
+    {
+        name: 'Tour Booking',
+        icon: 'fa-solid fa-umbrella-beach',
+        link: 'https://apkwan-natours.herokuapp.com',
+        highlights: ['Node.js', 'MongoDB', 'Vanilla Javascript', 'Modeling geospatial data', 'Restful APIS', 'Authentication', 'Stripe credit card payment'],
+        description: 'A beautiful travel agency website. Users may login, book tours and checkout.'
+    }, 
+    {
+        name: 'PropShop',
+        icon: 'fa-solid fa-shop',
+        link: 'https://apkwan-proshop.herokuapp.com',
+        highlights: ['React.js', 'Redux', 'Node.js', 'MongoDB', 'Bootstrap', 'Restful APIs', 'Authentication', 'Paypal payment'],
+        description: 'A custom eCommerce, users may register accounts, put their favorite items in the shopping cart and finally made a payment through Paypal.'
+    },
+    {
+        name: 'The Kepler Missions',
+        icon: 'fa-solid fa-rocket',
+        link: 'http://3.101.59.254:8000',
+        highlights: ['React.js', 'Node.js', 'MongoDB', 'Docker', 'Restful APIS', 'Authentication', 'AWS deployment'],
+        description: 'Discover the "habitable" planets across galaxy and maybe book tour there!!!'
+    }, 
+    {
+        name: 'Lets Go Places',
+        icon: 'fa-solid fa-camera',
+        link: 'https://apkwan-places.firebaseapp.com',
+        highlights: ['React.js', 'Node.js', 'MongoDB', 'Bootstrap', 'Restful APIs', 'Authentication', 'Pictures upload (Cloudinary)'],
+        description: 'Do you have any favorite places to share with us? Here is the place!'
+    },
+    {
+        name: 'Hotel Booking Business',
+        icon: 'fa-solid fa-hotel',
+        link: 'https://apkwan-hotel.firebaseapp.com',
+        highlights: ['React.js', 'Node.js', 'MongoDB', 'Bootstrap', 'Restful APIs', 'Authentication', 'Stripe credit card payment'],
+        description: 'This hotel website bridges the gap between support agents agents and customers. The website will take a whopping 20% cut of each transection.'
+    },
+    {
+        name: 'Developer Connector',
+        icon: 'fa-solid fa-blog',
+        link: 'https://apkwan-devconnector.herokuapp.com',
+        highlights: ['React.js', 'Redux', 'Node.js', 'MongoDB', 'Restful APIs', 'Authentication'],
+        description: 'Inspired by LinkedIn, users may register accounts and build their own profiles and post comments for the community.'
+    },
+    {
+        name: 'Expensify',
+        icon: 'fa-solid fa-receipt',
+        link: 'https://apkwan-expensify.herokuapp.com',
+        highlights: ['React.js (class component)', 'Redux', 'SASS', 'Jest testing', 'Restful APIS', 'Gmail login'],
+        description: 'This simple application help to you keep track of day to day spending. You will need a gmail account to login!!'
+    }, 
+    {
+        name: 'Realtor Website',
+        icon: 'fa-solid fa-house',
+        link: 'https://apkwan-realtor-grid.firebaseapp.com',
+        highlights: ['CSS Grid', 'SASS'],
+        description: 'Making a responsive website with CSS grid and SASS.'
+    }, 
+    {
+        name: 'Pong from the 70s',
+        icon: 'fa-solid fa-table-tennis-paddle-ball',
+        link: 'https://apkwan-pong.herokuapp.com',
+        highlights: ['Vanilla Javascript', 'Socket.IO', 'Node.js', 'HTML Canvas'],
+        description: '**** To play this nostalgic game, you will need two players on separate browsers. ***'
+    },
+];
---
+++ javascript/renderProjects.js
@@ -0,0 +1,69 @@
+class Projects {
+    constructor(project) {
+        this.proj = project;
+        this.creatProject();
+    }
+    
+    creatProject() {
+        const rootDiv = document.getElementById('projects');
+
+        const projectEl = document.createElement('div');
+        projectEl.classList.add('card', 'rounded-3', 'shadow-sm', 'mb-3');
+        rootDiv.append(projectEl);
+
+        // create card body
+        const projectBodyEl = document.createElement('div');
+        projectBodyEl.classList.add('card-body');
+        projectEl.append(projectBodyEl);
+
+        // create card body h4
+        const bodyH4 = document.createElement('h4');
+        bodyH4.classList.add('card-title', 'mb-3');
+        bodyH4.textContent = this.proj.name;
+        // create icon for h4
+        const h4Icon = document.createElement('i');
+        h4Icon.className = `${this.proj.icon} mr-2`;
+        bodyH4.insertAdjacentElement('afterbegin', h4Icon);
+        projectBodyEl.append(bodyH4);
+        
+        // create project link
+        const projLinkEl =  document.createElement('p');
+        projLinkEl.classList.add('p-2', 'mb-3', 'bg-dark', 'text-white');
+        const projLink = document.createElement('a');
+        projLink.className = ('text-white');
+        projLink.setAttribute('href', this.proj.link);
+        projLink.setAttribute('target', '_blank');
+        projLink.textContent = this.proj.link;
+        projLinkEl.append(projLink);
+        projectBodyEl.append( projLinkEl);
+
+        // create highlight container
+        const projectHighlightEl = document.createElement('div');
+        projectHighlightEl.classList.add('p-2', 'mb-3', 'bg-dark', 'text-white');
+        const h6Highlight = document.createElement('h6');
+        h6Highlight.textContent = 'Highlights'
+        // create highlight list
+        const ulHighlight = document.createElement('ul');
+        this.proj.highlights.forEach(highlight => {
+            const list = document.createElement('li');
+            list.textContent = highlight;
+            ulHighlight.append(list);
+        });
+        // append both h6 and ul to card body
+        projectHighlightEl.append(h6Highlight);
+        projectHighlightEl.append(ulHighlight);
+        projectBodyEl.append(projectHighlightEl);
+
+        // create card footer
+        const projectFooterEl = document.createElement('div');
+        projectFooterEl.classList.add('card-footer');
+        const footer =  document.createElement('p');
+        footer.classList.add('text-lead');
+        footer.textContent = this.proj.description;
+        projectFooterEl.append(footer);
+        projectBodyEl.append(projectFooterEl);
+
+    }
+}
+
+projectList.forEach(proj => new Projects(proj));
